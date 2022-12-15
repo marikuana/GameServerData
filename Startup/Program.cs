27bd5973ba9f602b562787dcb86918832b61f15a
Marikuana <marians22092000@gmail.com>
2022-12-15 11:10:35 +0200

    Add Startup project

---
+++ Startup/Program.cs
@@ -0,0 +1,31 @@
+﻿using GameServerData;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Startup
+{
+    public class Program
+    {
+        public static IHostBuilder CreateHostBuilder(string[] args)
+        {
+            return Host.CreateDefaultBuilder(args)
+                .ConfigureServices(ConfigureServices);
+        }
+
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args)
+                .Build()
+                .Start();
+        }
+
+        public static void ConfigureServices(HostBuilderContext host, IServiceCollection services)
+        {
+            services.AddDbContext<Db>(options =>
+            {
+                options.UseSqlServer();
+            });
+        }
+    }
+}
